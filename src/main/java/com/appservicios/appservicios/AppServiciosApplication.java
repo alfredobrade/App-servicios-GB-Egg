4f6d60acc8038fccbb4416cd720fd3f7f942f508
Alfredo Brade <alfredobrade@gmail.com>
2023-03-07 20:37:03 -0300

    iniciando el proyecto

--- src/main/java/com/appservicios/appservicios/AppServiciosApplication.java
+++ src/main/java/com/appservicios/appservicios/AppServiciosApplication.java
@@ -8,6 +8,8 @@ public class AppServiciosApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hola grupo B");
+
+		//comento el inicio de la app spring hasta que configuremos la base de datos
 		//SpringApplication.run(AppServiciosApplication.class, args);
 	}
 
