a3607b62135e0236e3b523fc757ec76e9a8a6589
Alfredo Brade <alfredobrade@gmail.com>
2023-03-07 20:27:41 -0300

    iniciando el proyecto

---
+++ src/main/java/com/appservicios/appservicios/AppServiciosApplication.java
@@ -0,0 +1,14 @@
+package com.appservicios.appservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServiciosApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hola grupo B");
+		//SpringApplication.run(AppServiciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appservicios/appservicios/AppServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appservicios.appservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
